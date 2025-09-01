d4a620820fd1a4ea25cc994ba5e9ada1a688efa5
Hassan Abdelnaby <hassan.abdelnaby69@gmail.com>
2025-09-02 02:46:11 +0300

    Add action method for creating a Department

--- assignment1/Controllers/DepartmentController.cs
+++ assignment1/Controllers/DepartmentController.cs
@@ -1,4 +1,5 @@
 ﻿using assignment1.Models;
+using assignment1.ViewModel;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -16,6 +17,34 @@ namespace assignment1.Controllers
             return View("Index", departments);
 
         }
-        
+        public IActionResult Add()
+        {
+            DepartmentCreateViewModel data = new DepartmentCreateViewModel
+            {
+                AllInstructors = context.instructors.ToList(),
+                AllCourses = context.courses.ToList(),
+                AllTrainees = context.trainees.ToList()
+            };
+            return View("Add",data);
+        }
+        public IActionResult Create(DepartmentCreateViewModel data)
+        {
+            if(data.Name == null || data.Manager == null || data.SelectedCourseIds==null || data.SelectedTraineeIds==null || data.SelectedInstructorIds==null)
+            {
+                
+                return RedirectToAction("Add");
+            }
+            Department department = new Department
+            {
+                Name = data.Name,
+                Manager = data.Manager,
+                Instructors = context.instructors.Where(i => data.SelectedInstructorIds.Contains(i.Id)).ToList(),
+                Courses = context.courses.Where(c => data.SelectedCourseIds.Contains(c.Id)).ToList(),
+                Trainees = context.trainees.Where(t => data.SelectedTraineeIds.Contains(t.Id)).ToList()
+            };
+            context.departments.Add(department);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
 }
---
+++ assignment1/ViewModel/DepartmentCreateViewModel.cs
@@ -0,0 +1,21 @@
+﻿using assignment1.Models;
+
+namespace assignment1.ViewModel
+{
+    public class DepartmentCreateViewModel
+    {
+
+        public string Name { get; set; }
+        public string Manager { get; set; }
+
+       
+        public List<int> SelectedInstructorIds { get; set; }
+        public List<int> SelectedCourseIds { get; set; }
+        public List<int> SelectedTraineeIds { get; set; }
+
+
+        public List<Instructor> AllInstructors { get; set; }
+        public List<Course> AllCourses { get; set; }
+        public List<Trainee> AllTrainees { get; set; }
+    }
+}
