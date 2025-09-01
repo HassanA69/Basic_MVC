060788a3ed3c6be1d25cea770cea9d2868b8e229
Hassan Abdelnaby <hassan.abdelnaby69@gmail.com>
2025-09-02 00:28:32 +0300

    add department Controller and  view departments action

---
+++ assignment1/Controllers/DepartmentController.cs
@@ -0,0 +1,21 @@
+﻿using assignment1.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace assignment1.Controllers
+{
+    public class DepartmentController : Controller
+    {
+        AppDbContext context = new AppDbContext();
+        public IActionResult Index()
+        {
+            List<Department> departments = context
+                                         .departments
+                                         .Include(d => d.Instructors)
+                                         .ToList();
+            return View("Index", departments);
+
+        }
+        
+    }
+}
